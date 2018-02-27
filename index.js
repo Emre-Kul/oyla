8be8e42651cb93be0cdb8f23d5fe8ad1aa13951f
Emre-Kul <emrekul35@gmail.com>
2018-02-27 18:47:53 +0300

    views created

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.set('view engne','ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 const server = app.listen(process.env.PORT || 8080, () => {
     const host = server.address().address;
