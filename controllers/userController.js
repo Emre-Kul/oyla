cf6f55ad8e27ce6ee62532ca049e9a153817b02e
Emre-Kul <emrekul35@gmail.com>
2018-03-13 20:44:09 +0300

    very bad register functionality added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,28 @@
+const models = require('../models/');
+
 exports.loginPost = function (req, res) {
     const { email, password } = req.body;
     res.send(`Will Login With ${email} --- ${password}`);
 }
 
 exports.registerPost = function (req, res) {
-    const {username, email, password, repassword} = req.body;
-    res.send(`Will Register ${username} ${email} ${password} ${repassword}`)
+    const { username, email, password, repassword } = req.body;
+    
+    if(password !== repassword){
+        res.send("Passwords Not Same!");
+        return;
+    }
+
+    if(password.length > 10 || password.length < 4){//i will delete this magic numbers
+        res.send("Password must be beetween 4 and 10");
+        return;
+    }
+    
+    models.User.create({username : username,password : password,email : email,token : 'NONE'}).then((user) => {
+        res.render('pages/index');
+    }).catch((err) => {
+        res.status(404).send("ERR");
+        console.log(err);
+    });
+
 }
