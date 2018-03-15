60bf9ccdfa61274f66d7066356102199b4284d0b
Emre-Kul <emrekul35@gmail.com>
2018-03-15 17:44:08 +0300

    improvements for frontend login system input control

--- public/js/authControl.js
+++ public/js/authControl.js
@@ -1,33 +1,57 @@
 'use_strict'
 var rules = {
     username: {
-        minLength: 5
+        minLength: 5,
+        maxLength: 50
     },
     email: {
-        minLength: 5
+        minLength: 5,
+        maxLength: 100
+    },
+    password: {
+        minLength: 6,
+        maxLength: 25
+    },
+    rePassword: {
+        minLength: 6,
+        maxLength: 25
     }
+}
+var controlInputLength = function(value,selector,minLength,maxLength){
+    if(value.length < minLength || value.length > maxLength){
+        $(selector).addClass("is-invalid");
+        return false;
+    }
+    $(selector).removeClass("is-invalid");
+    $(selector).addClass("is-valid")
+    return true;
 }
 
 var handleUsernameValidationInput = function (e) {
     var username = e.target.value;
-    if (username.length < rules.username.minLength) {
-        $(this).addClass("is-invalid");
-    } else {
-        $(this).removeClass("is-invalid");
-    }
+    controlInputLength(username,this,rules.username.minLength,rules.username.maxLength);
 }
 
 var handleEmailValidationInput = function (e) {
     var email = e.target.value;
-    if (email.length < rules.email.minLength) {
-        $(this).addClass("is-invalid");
-    } else {
-        $(this).removeClass("is-invalid");
-    }
+    controlInputLength(email,this,rules.email.minLength,rules.email.maxLength);
+}
+
+var handlePasswordValidationInput = function (e) {
+    var password = e.target.value;
+    controlInputLength(password,this,rules.password.minLength,rules.password.maxLength);
+}
+
+var handleRePasswordValidationInput = function (e) {
+    var rePassword = e.target.value;
+    controlInputLength(rePassword,this,rules.rePassword.minLength,rules.rePassword.maxLength);
 }
 
 $(document).ready(function () {
     $('#username').on("input", handleUsernameValidationInput);
     $('#email').on("input", handleEmailValidationInput);
+    $('#password').on("input", handlePasswordValidationInput);
+    $('#repassword').on("input", handleRePasswordValidationInput);
+
     console.log("rEADY");
 });
