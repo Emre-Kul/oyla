09394d91908167cc095cf8374a22b413782b968a
Emre-Kul <emrekul35@gmail.com>
2018-03-15 17:58:07 +0300

    user model password salted hash added and some improvements

--- models/user.js
+++ models/user.js
@@ -1,20 +1,39 @@
 'use strict';
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     email: {
       type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
       validate: {
         isEmail: true
       }
     },
     token: DataTypes.STRING
   }, {
-    underscored: true
-  });
-  User.associate = function(models) {
+      hooks: {
+        beforeCreate: (user) => {
+          const salt = bcrypt.genSaltSync();
+          user.password = bcrypt.hashSync(user.password, salt);
+        }
+      },
+      underscored: true
+    });
+
+
+  User.associate = function (models) {
     models.User.hasMany(models.UserMeta);
   };
+
   return User;
 };
