ef9f81c620425f5dde62c7b897a04f910ee760f9
Emre-Kul <emrekul35@gmail.com>
2018-03-30 18:03:06 +0300

    profile can update via form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,35 @@
 const models = require('../models');
 
 exports.userProfileGet = function (req, res) {
-    res.render('pages/userProfile');
+    let userProfileData;
+    models.UserProfile.findOne({ user_id: req.session.user.id }).then((userProfile) => {
+        res.render('pages/userProfile', {
+            userProfile: userProfile.dataValues
+        });
+    }).catch((err) => {
+        console.log(err);
+        res.send("Some Error Accured");
+    });
 }
 
 exports.userProfilePost = function (req, res) {
-    res.render('pages/userProfile');
+    const { fullname, income, sex, degree } = req.body;
+    models.UserProfile.update(
+        {
+            fullname : fullname,
+            income : income,
+            sex : sex,
+            degree : degree
+        },
+        {
+            where : {
+                user_id : req.session.user.id
+            }
+        }
+    ).then((result) => {
+        res.redirect('/user/profile');
+    }).catch((err) => {
+        res.send(err.errors[0].message);
+    });
+    
 }
