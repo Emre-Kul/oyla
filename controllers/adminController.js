99331b414be385c727bffcc37a13dcbdf250378b
Emre-Kul <emrekul35@gmail.com>
2018-04-21 14:57:01 +0300

    search and pageing added to admin controllers

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,23 +1,59 @@
 const models = require('../models/');
+const Op = require('sequelize').Op;
+
+const SQL_LIMIT = 2;
 
 exports.dashboardGet = function (req, res) {
     res.render('pages/admin/dashboard');
 }
 
 exports.userGet = function (req, res) {
-    models.User.findAll().then((users) => {
-        let userValues = users.map((user) =>{
+    let { search, page } = req.query;
+    search = (typeof search === 'undefined') ? '%' : `%${search}%`;
+    page = (typeof page === 'undefined') ? 0 : parseInt(page);
+
+    models.User.findAll(
+        {
+            where: {
+                username: {
+                    [Op.like]: search
+                }
+            },
+            offset: SQL_LIMIT * page,
+            limit: SQL_LIMIT
+        }
+    ).then((users) => {
+        let userValues = users.map((user) => {
             return user.dataValues;
         })
         res.render('pages/admin/user', { users: userValues });
-    }).catch(e => console.log(e));
+    }).catch((e) => {
+        console.log(e);
+        res.redirect('/error/500');
+    });
 }
 
 exports.surveyGet = function (req, res) {
-    models.Survey.findAll().then((surveys) => {
-        let surveyValues = surveys.map((survey) =>{
+    let { search, page } = req.query;
+    search = (typeof search === 'undefined') ? '%' : `%${search}%`;
+    page = (typeof page === 'undefined') ? 0 : parseInt(page);
+    models.Survey.findAll(
+        {
+            where: {
+                title: {
+                    [Op.like]: search
+                }
+            },
+            offset: SQL_LIMIT * page,
+            limit: SQL_LIMIT
+        }
+    ).then((surveys) => {
+        let surveyValues = surveys.map((survey) => {
             return survey.dataValues;
         });
         res.render('pages/admin/survey', { surveys: surveyValues });
-    }).catch(e => console.log(e));
+    }).catch((e) => {
+        console.log(e);
+        res.redirect('/error/500');
+    });
 }
