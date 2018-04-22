67472af45e8df25a0ee6b955846d7920f548d00f
unknown <ksonmezoz@outlook.com>
2018-04-22 16:24:51 +0300

    Invalid message for register.ejs added

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,33 +28,13 @@ exports.registerGet = function (req, res) {
 exports.registerPost = function (req, res) {
     const { username, email, password, repassword } = req.body;
 
-    models.User.findOne({ where: { email: email } }).then((user) => {
-        if (user != null && email === user.email) {
-            res.render('pages/auth/register',{error: {message: "Email has been already taken !"}});
-            return;
-        }
-    }).catch((err) => {
-        res.status(404).send("Error Accured!");
-        console.log(err);
-    });    
-
-    models.User.findOne({ where: { username: username } }).then((user) => {
-        if (user != null && username === user.username) {
-            res.render('pages/auth/register',{error: {message: "Username has been already taken !"}});
-            return;
-        }
-    }).catch((err) => {
-        res.status(404).send("Error Accured!");
-        console.log(err);
-    });
-
     if (password !== repassword) {
         res.render('pages/auth/register',{error: {message: "Passwords are not same !"}});
         return;
     }
 
     if (password.length > 10 || password.length < 4) {//i will delete this magic numbers
-        res.render('pages/auth/register',{error: {message: "Password must be between 4 and 10", code: "002"}});
+        res.render('pages/auth/register',{error: {message: "Password must be between 4 and 10"}});
         return;
     }
 
@@ -68,7 +48,11 @@ exports.registerPost = function (req, res) {
                 res.status(404).send(err.errors[0].message);//this is not good find a better way
             });
     }).catch((err) => {
-        res.status(404).send(err.errors[0].message);
+        let msg = err.errors[0].path;
+        msg = msg[0].toUpperCase() + msg.substring(1);
+        msg += " has been already taken !";
+        res.render('pages/auth/register',{error: {message: msg}});
     });
+    
 
 }
