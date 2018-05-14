25a14284274516cd04a2300b87ddb3a3573daa2a
unknown <ksonmezoz@outlook.com>
2018-05-14 23:44:50 +0300

    Latest 5 surveys' info have been sent to index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models');
 
 const authHelper = require('../helpers/authHelper.js');
 
 router.get('/',authHelper.isNotLogined,(req, res) => {
-    res.render('pages/index');
+    models.Survey.findAndCountAll({
+        order: [["created_at", "DESC"]],
+        offset: 0,
+        limit: 5
+    }).
+        then((surveys) => {
+            if (surveys) {
+                let surveyList = surveys.rows.map((survey) => {
+                    let surveyObj = survey.dataValues;
+                    return surveyObj;
+                });
+                res.render('pages/index.ejs', { surveys: surveyList });
+            }
+            else
+                res.render('pages/index.ejs', { surveys: [] });
+        }).
+        catch((err) => {
+            res.send("ERROR")
+            console.log(err);
+        });
 });
 
 module.exports = router;
